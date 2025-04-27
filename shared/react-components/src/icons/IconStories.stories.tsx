49df0ca8b1329109d233c60056cbbdb4518c8a5b
shadowrunner <pieroramirez810@gmail.com>
2025-04-27 14:20:52 -0500

    chore: added new icons

---
+++ shared/react-components/src/icons/IconStories.stories.tsx
@@ -0,0 +1,50 @@
+import { Meta, StoryObj } from "@storybook/react";
+import DevopsDaysLogo from "./DevopsDaysLogo";
+import HandShake from "./HandShake";
+import LigthBulb from "./LigthBulb";
+import Stocks from "./Stocks";
+
+const meta = {
+  title: "Icons",
+  component: DevopsDaysLogo, // Default component for meta, individual stories will override
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof DevopsDaysLogo>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const DevopsDaysLogoStory: Story = {
+  args: {
+  },
+  render: (args) => <DevopsDaysLogo  {...args} />,
+};
+
+export const HandShakeStory: Story = {
+  args: {
+    className: "bg-cyan-base rounded-md p-1",
+    width: 32,
+    height: 32,
+  },
+  render: (args) => <HandShake {...args} />,
+};
+
+export const LigthBulbStory: Story = {
+  args: {
+    className: "bg-cyan-base rounded-md p-1",
+    width: 32,
+    height: 32,
+  },
+  render: (args) => <LigthBulb {...args} />,
+};
+
+export const StocksStory: Story = {
+  args: {
+    className: "bg-cyan-base rounded-md p-1",
+    width: 32,
+    height: 32,
+  },
+  render: (args) => <Stocks {...args} />,
+};
