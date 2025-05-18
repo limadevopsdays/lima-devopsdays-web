0f13128241e93b2ac5eb872d65adbe1afc52df6e
Paul Chavez <paulchavezromero20@gmail.com>
2025-05-17 21:02:53 -0500

    feat: add getGlobalConfig method to LocalGlobalConfigService

--- apps/new-web/src/services/LocalGlobalConfigService.ts
+++ apps/new-web/src/services/LocalGlobalConfigService.ts
@@ -2,8 +2,17 @@ import { injectable } from "inversify";
 import { IGlobalConfig } from "./IGlobalConfig";
 
 @injectable()
-export class LocalGlobalConfigService implements IGlobalConfig{
+export class LocalGlobalConfigService implements IGlobalConfig {
   getPaymentExternalLink(): Promise<string> {
     return Promise.resolve(process.env.NEXT_PUBLIC_PAYMENT_EXTERNAL_LINK ?? "");
+  }
+  getGlobalConfig(): Promise<any> {
+    return Promise.resolve({
+      fields: {
+        name: "Comprar Entradas",
+        paymentExternalLink: process.env.NEXT_PUBLIC_PAYMENT_EXTERNAL_LINK ?? "",
+        gtmId: process.env.NEXT_PUBLIC_GTM_ID ?? "",
+      },
+    });
   }
 }
